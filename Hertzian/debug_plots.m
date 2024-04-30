898d4b7ba4e9c8182c84e1b018c982f4e7e1a19b
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-04-30 10:53:38 -0400

    Added portion for histogram of frquencies

--- Hertzian/debug_plots.m
+++ Hertzian/debug_plots.m
@@ -14,9 +14,25 @@ xlabel('X');
 ylabel('Y');
 zlabel('Z');
 
+nn = 30
 figure; % Create a new figure window
-stem(freq_vector, abs(normalized_fft_data(index_vector)) * 2); % Plot the frequency spectrum
+stem(frequency_spectra{nn}.Frequencies, frequency_spectra{nn}.Amplitudes * 2); % Plot the frequency spectrum
 xlabel('Frequency (Hz)'); % Label the x-axis
 ylabel('Amplitude'); % Label the y-axis
-title('Frequency Spectrum'); % Title for the plot
+title(sprintf('Frequency Spectrum for Particle %d', frequency_spectra{nn}.ParticleIndex), 'FontSize', 12); % Title for the plot
 grid on; % Turn on the grid for easier visualization
+
+% Collect all frequencies into one array
+all_frequencies = [];
+for k = 1:numel(frequency_spectra)
+    all_frequencies = [all_frequencies, frequency_spectra{k}.Frequencies];
+end
+
+% Plot histogram using hist
+figure;
+[n, x] = hist(all_frequencies, 40);  % 50 bins
+bar(x, n);  % Plot as bar chart
+title('Histogram of Frequencies','FontSize', 12);
+xlabel('Frequency (Hz)');
+ylabel('Count');
+grid on;
